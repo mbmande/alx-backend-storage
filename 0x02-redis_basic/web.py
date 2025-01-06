ef66c5d7fe9d3e690d3a2c49c5025d1b57028116
mbmande <mbsmande@gmail.com>
2025-01-06 19:47:55 +0000

    web updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """In this tasks, we will implement a get_page function
 (prototype: def get_page(url: str) -> str:). The core of
 the function is very simple. It uses the requests module
